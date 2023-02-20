8f4e2c672007b0bf2ab694f64675140e339f923f
dionisprime <112644498+dionisprime@users.noreply.github.com>
2023-02-20 12:06:17 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = ReactDOMClient.createRoot(document.getElementById('root'))
+app.render(<App />)
